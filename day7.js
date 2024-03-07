d6ddca0f002c0b25620651029a0cd071045737a8
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-03-08 04:06:13 +0500

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,19 @@
+//                                                                    TASK NO 1
+//Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+//Explain & TIP: Arrays have a length property that tells you how many items are in the list. Use this to inform your guests how big the party will be
+//let guests: string[] = ["Huzaifa", "Haseeb", "Shariq", "Abdullah", "Muhib" , "Usman"];
+//console.log(`I am inviting ${guests.length} people to dinner.`);
+//                                                                    TASK NO 2
+//Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+//Explain & TIP: Arrays are perfect for keeping a collection of similar items. Choose a theme you like and list several examples
+//let cities :string[] =["Karach","Hyderabad","Islamabad","lahore"]
+//console.log("Cities i would like to visit" ,cities);
+//                                                                     TASK NO 3
+//Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+//Explain & TIP: Objects in TypeScript can store various related data about a single entity. Think of an object as a container for characteristics of something, like a book or a car
+var book = {
+    bookname: "Atomic Habit",
+    Author: "James Clear",
+    Releasedate: 2018
+};
+console.log("book information : ".concat(book.bookname, ", book Author : ").concat(book.Author, " , Book release date : ").concat(book.Releasedate));
