f49ee7ff7ee137441a2ea73de27b5046e1050118
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-03-12 18:08:56 +0500

    Add files via upload

---
+++ day9.js
@@ -0,0 +1,60 @@
+//                                               TASK  NO 1
+//Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+//Explain & TIP: Use an if statement to check the alien's color. This introduces you to simple conditionals.
+// let alien_color = "red"
+// if(alien_color=="red"){
+//     console.log("the alien colour is red")
+// }
+// let alien_color = "green"
+// if (alien_color == "yellow"){
+//     console.log("the output is blank")
+// }
+//                                               TASK NO 2
+//Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+//Explain & TIP: Expanding on the previous exercise, if-else chains allow you to handle multiple conditions.
+// answer in if statement
+// let alien_color = "red"
+// if (alien_color= "red"){
+//     console.log("alien color is red")
+// }else{
+//     console.log('sorry i could not find your alien colour')
+// }
+// answer in else statement
+// let alien_color = "yellow";
+// if(alien_color == "red"){
+//     console.log("the alien colour is yellow")
+// }else{
+//     console.log("sorry i couldnot find your alien colour")
+// }
+//                                            TASK NO 3
+// Question 27: Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
+// Explain & TIP: This exercise introduces you to handling multiple specific conditions in an if-else chain.
+// green alien color version
+// let alien_color = "green"
+// if(alien_color == "green"){
+//     console.log("the alien color is yellow")
+// }else if(alien_color == "yellow"){
+//     console.log("the alien color is yellow")
+// }else{
+//     console.log("the alien color is red")
+// }
+//  yellow alien color version 
+// let alien_color = "yellow"
+// if(alien_color == "green"){
+//     console.log("the alien color is yellow")
+// }else if(alien_color == "yellow"){
+//     console.log("the alien color is yellow")
+// }else{
+//     console.log("the alien color is red")
+// }
+//  red alien color version
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("the alien color is yellow");
+}
+else if (alien_color == "yellow") {
+    console.log("the alien color is yellow");
+}
+else {
+    console.log("the alien color is red");
+}
