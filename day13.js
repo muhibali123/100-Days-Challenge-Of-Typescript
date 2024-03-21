b8de51035ff353f26ca3114fc8646cd15c4a2aeb
MuhibALiSiddiqui <162018565+muhibali123@users.noreply.github.com>
2024-03-21 21:24:30 +0500

    Add files via upload

---
+++ day13.js
@@ -0,0 +1,33 @@
+//                                                      TASK NO 1
+// Question 37: Large Shirts: Default values in make_shirt().
+// Explain & TIP: Modify functions to have default parameters, simplifying calls for common use cases. This introduces default function parameters.
+// function make_shirt(
+//   size: string = "large",
+//   message: string = "I love Typescript "
+// ) {
+//   console.log(
+//     `Making a ${size} t-shirt with the message "${message}" printed on it. `
+//   );
+// }
+// make_shirt(); // Default large and message
+// make_shirt("medium");
+// make_shirt("small", "i love Python");
+//                                                    TASK NO 2
+// Question 38: Cities: Describing cities with a function.
+// Explain & TIP: Use functions with default parameters to handle cases where certain data might not change often. This helps in making your code more flexible.
+function cities(city, country) {
+    if (country === void 0) { country = "Pakistan"; }
+    console.log("the city name is ".concat(city, " and it is presented in ").concat(country));
+}
+cities("Karachi");
+cities("Hyderabad");
+cities("Dubai", "UAE");
+//                                                    TASK NO 3
+// Question 39: City Names: Formatting city-country pairs.
+// Explain & TIP: Returning formatted strings from functions can simplify data presentation. This exercise practices string formatting and returning values from functions.
+// function cities(city: string, country: string): string {
+//   return `${city},${country}`;
+// }
+// console.log(cities("New York City", "USA"));
+// console.log(cities("Paris", "France"));
+// console.log(cities("Beijing", "China"));
